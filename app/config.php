1acea4362742bc573aad27ccf0d0ee94b3058700
MarinAmidzic <73863784+MarinAmidzic@users.noreply.github.com>
2021-03-01 21:22:14 +0100

    Add files via upload

--- app/config.php
+++ app/config.php
@@ -1,12 +1,22 @@
 <?php
-
-return [
-    'url'=>'http://zavrsnirad.hr/',
-    'nazivApp'=>'Fitness',
-    'baza'=>[
+$dev=$_SERVER['REMOTE_ADDR']==='127.0.0.1' ? true : false;
+if($dev){
+    $baza=[
         'server'=>'localhost',
         'baza'=>'fitnessdatabase2',
         'korisnik'=>'edunova',
         'lozinka'=>'edunova'
-    ]
+    ];
+}else{
+    $baza=[
+        'server'=>'localhost',
+        'baza'=>'morfej_pp22',
+        'korisnik'=>'morfej_edunova',
+        'lozinka'=>'edunova123.'
+    ];
+}
+return [
+    'url'=>'http://zavrsnirad.hr/',
+    'nazivApp'=>'Fitness',
+    'baza'=>$baza
 ];
